de38a5e96b7cd01d49b96622259e4f109b39b811
Amr Hares <amr.s.hares@gmail.com>
2026-03-22 19:10:43 +0200

    fix relative imports from modules

--- simulated_shopping/services/order_service.py
+++ simulated_shopping/services/order_service.py
@@ -1,4 +1,4 @@
-from ..models import Order
+from models import Order
 
 class OrderService:
 
--- simulated_shopping/services/user_service.py
+++ simulated_shopping/services/user_service.py
@@ -1,6 +1,6 @@
 import logging
 import json
-from ..models import User
+from models import User
 
 logger = logging.getLogger(__name__)
 
@@ -11,15 +11,18 @@ class UserService:
 
     def load_user_data(self):
         try:
-            with open(self.users_path, 'r') as f:
+            with open(self._users_path, 'r') as f:
                 self.__users = json.load(f)
-            logger.info(f"Loaded user data from {self.users_path}")
+            logger.info(f"Loaded user data from {self._users_path}")
         except FileNotFoundError:
-            logger.exception(f"users file {self.users_path} not found. Starting with empty user base.")
-            self._users = {}
+            logger.exception(f"users file {self._users_path} not found. Starting with empty user base.")
+            self.__users = {}
+
+    def view_users(self):
+        return self.__users
 
     def get_user_data(self, user_id):
-        return self._users.get(user_id, False)
+        return self.__users.get(user_id, False)
 
     def view_user_data(self, user_id):
         print(User(**self.__users[user_id]))
