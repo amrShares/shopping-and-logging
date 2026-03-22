446cba93f956b61c1a475d99b37d4b04b5ccf095
Amr Hares <amr.s.hares@gmail.com>
2026-03-22 19:00:04 +0200

    add order reversion

--- simulated_shopping/services/order_service.py
+++ simulated_shopping/services/order_service.py
@@ -10,17 +10,20 @@ class OrderService:
     def create_order(self, user_id, cart, product_lookup):
 
         total_price = 0
-
+        items_added  = 0
         for product_id, quantity in cart.items.items():
 
-            product = product_lookup(product_id)
+            product = product_lookup[product_id]
 
-            self.inventory_service.reduce_stock(
+            if self.inventory_service.reduce_stock(
                 product_id,
                 quantity
-            )
-
-            total_price += product.price * quantity
+            ):
+                total_price += product.price * quantity
+                items_added += 1
+            else:
+                self.revert_order(user_id, cart, product_lookup, items_added)
+                return False
 
         order = Order(
             order_id=self.order_counter,
@@ -32,3 +35,17 @@ class OrderService:
         self.order_counter += 1
 
         return order
+
+    def revert_order(self, user_id, cart, product_lookup, items_added):
+
+
+        for product_id, quantity in cart.items.items():
+
+            self.inventory_service.fill_stock(
+                product_id,
+                quantity
+            )
+            items_added -= 1
+
+            if items_added == 0:
+                return
